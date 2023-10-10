cc0812bc868ebb5762654dd5270067bdd1ea0468
Nikhil <nikhilcr794@gmail.com>
2023-10-10 11:17:42 +0530

    Need to change the way the image loads

--- src/components_ayurhub/ImageSearchInput.js
+++ src/components_ayurhub/ImageSearchInput.js
@@ -3,7 +3,32 @@ import InputBase from "@mui/material/InputBase";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
-const ImageSearchInput = () => {
+import { useRef, useState } from "react";
+const ImageSearchInput = (props) => {
+  const [query, setQuery] = useState("");
+
+  const queryChangeHandler = (event) => {
+    setQuery(event.target.value);
+  };
+  async function searchHandler(event) {
+    event.preventDefault();
+    const url = "http://localhost:5000/plant/" + query;
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+
+    const item = {
+      path: data.path,
+      text: data.text,
+    };
+    props.onSearchPressed(item);
+  }
+
   return (
     <Paper
       component="form"
@@ -19,8 +44,15 @@ const ImageSearchInput = () => {
         sx={{ ml: 1, flex: 1 }}
         placeholder="Search AyurHub"
         inputProps={{ "aria-label": "Search Ayurhub" }}
+        value={query}
+        onChange={queryChangeHandler}
       />
-      <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+      <IconButton
+        type="button"
+        sx={{ p: "10px" }}
+        aria-label="search"
+        onClick={searchHandler}
+      >
         <SearchIcon />
       </IconButton>
       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
--- src/pages/AyurHub.js
+++ src/pages/AyurHub.js
@@ -3,7 +3,18 @@ import classes from "./AyurHub.module.css";
 import ImageSearchInput from "../components_ayurhub/ImageSearchInput";
 import SuggestionPanel from "../components_ayurhub/SuggestionPanel";
 import AyurHubIcon from "../assets/ayurhublogo.png";
+import { useState } from "react";
+// import "../../../../Indian Medicinal Leaves Image Datasets";
+
 const AyurHub = () => {
+  const [content, setContent] = useState({
+    path: "",
+    text: "",
+  });
+  const searchHandler = (content) => {
+    console.log("../assets/dataset/" + content.path);
+    setContent(content);
+  };
   return (
     <div className={classes.hubMain}>
       <aside className={classes.sideMenu}>
@@ -13,15 +24,15 @@ const AyurHub = () => {
       </aside>
       <div className={classes.containerMain}>
         <div className={classes.containerLeft}>
-          <ImageSearchInput />
+          <ImageSearchInput onSearchPressed={searchHandler} />
           <SuggestionPanel />
           <div className={classes.suggestionPanel}></div>
         </div>
         <div className={classes.containerRight}>
           <div className={classes.imageData}>
-            <img src={AyurHubIcon} />
+            <img src={"../assets/dataset/" + content.path + "/339.jpg"} />
           </div>
-          <p className={classes.ImageDescription}>Welcome to AyurHub</p>
+          <p className={classes.ImageDescription}>{content.text}</p>
         </div>
       </div>
     </div>
